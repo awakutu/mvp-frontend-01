a2814328d93b75061b5ca783be2a8c6b1c68f9fb
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-22 08:21:31 +0700

    implementation portfolio page

--- src/components/Profile/Portofolio.jsx
+++ src/components/Profile/Portofolio.jsx
@@ -1,7 +1,88 @@
 export default function Portofolio() {
-    return (
-        <main className="mt-4">
-            <h1>Ini adalah laman portofolio</h1>
-        </main>
-    )
+  return (
+    <main className="container-fluid">
+      <div className="card my-4">
+        <div className="card-body">
+          <h5>Experience</h5>
+          <span className="d-flex justify-content-between">
+            <p>Customer Relation di Bank BNI </p>
+            <p>
+              2017-2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-between">
+            <p>Customer Relation di Bank BRI </p>
+            <p>
+              2017-2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-center mt-3">
+            <button className="btn text-primary ">Tambah Experience</button>
+          </span>
+        </div>
+      </div>
+      <div className="card my-4">
+        <div className="card-body">
+          <h5>Expertise/Knowledges</h5>
+          <span className="d-flex justify-content-between">
+            <p>Public Speaking</p>
+            <p>
+              Advance <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-between">
+            <p>Akuntansi </p>
+            <p>
+              Intermediate <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-center mt-3">
+            <button className="btn text-primary ">Tambah </button>
+          </span>
+        </div>
+      </div>
+      <div className="card my-4">
+        <div className="card-body">
+          <h5>Kontribusi Artikel</h5>
+          <span className="d-flex justify-content-between">
+            <p>Mengatur Keuangan di Masa Pandemi </p>
+            <p>
+              Di Post 20 November 2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-between">
+            <p>Mengatur Keuangan di masa Pandemi </p>
+            <p>
+              Di Post 20 November 2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+        </div>
+      </div>
+      <div className="card my-4">
+        <div className="card-body">
+          <h5>Grup Proyek Inovasi</h5>
+          <p className="font-weight-bold">Banking Social App</p>
+          <p>Merupakan Grup Project Banking Social App, yang dibuat dalam Rangka Meningkatkan Kepedulian Para Penggiat Bank dalam Hubungan Social Bermasyarakat</p>
+          <span className="d-flex justify-content-between">
+            <p>Customer Relation di Bank BNI </p>
+            <p>
+              2017-2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-between">
+            <p>Customer Relation di Bank BRI </p>
+            <p>
+              2017-2020 <i className="fa fa-pencil" />
+            </p>
+          </span>
+          <span className="d-flex justify-content-center mt-3">
+            <button className="btn text-primary ">Tambah Experience</button>
+          </span>
+        </div>
+      </div>
+   
+   
+   
+    </main>
+  );
 }
